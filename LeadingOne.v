a051dfe10ff456740d552756f54d3a3fdafa290c
黃俊誠 <s111060003@m111.nthu.edu.tw>
2025-02-08 18:04:46 +0800

    Update LeadingOne.v

--- LeadingOne.v
+++ LeadingOne.v
@@ -1,29 +1,32 @@
 module LeadingOne(
-  input  wire [8:0] a,
-  output reg  [3:0] index
+  input  wire [8:0] number_i,
+  output reg  [3:0] index_o
 );
 
+  /////////////////////////
+  // Combinational Logic //
+  /////////////////////////
   always @* begin
-    if (a[8] == 1) begin
-      index = 4'd8;
-    end else if (a[7] == 1) begin
-      index = 4'd7;
-    end else if (a[6] == 1) begin
-      index = 4'd6;
-    end else if (a[5] == 1) begin
-      index = 4'd5;
-    end else if (a[4] == 1) begin
-      index = 4'd4;
-    end else if (a[3] == 1) begin
-      index = 4'd3;
-    end else if (a[2] == 1) begin
-      index = 4'd2;
-    end else if (a[1] == 1) begin
-      index = 4'd1;
-    end else if (a[0] == 1) begin
-      index = 4'd0;
+    if (number_i[8] == 1) begin
+      index_o = 4'd8;
+    end else if (number_i[7] == 1) begin
+      index_o = 4'd7;
+    end else if (number_i[6] == 1) begin
+      index_o = 4'd6;
+    end else if (number_i[5] == 1) begin
+      index_o = 4'd5;
+    end else if (number_i[4] == 1) begin
+      index_o = 4'd4;
+    end else if (number_i[3] == 1) begin
+      index_o = 4'd3;
+    end else if (number_i[2] == 1) begin
+      index_o = 4'd2;
+    end else if (number_i[1] == 1) begin
+      index_o = 4'd1;
+    end else if (number_i[0] == 1) begin
+      index_o = 4'd0;
     end else begin
-      index = -1;
+      index_o = -1;
     end
   end
 
